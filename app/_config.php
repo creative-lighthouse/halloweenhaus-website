3727ff5fde49197cd5e8f01caf7c6fe60d8a6cd7
Steffen Kahl <stevenpaw209@gmail.com>
2025-07-07 02:02:48 +0200

    [~] Removing all imports and trying to migrate to sass6

--- app/_config.php
+++ app/_config.php
@@ -12,3 +12,4 @@ Member::set_password_validator($validator);
 i18n::set_locale('de_DE');
 
 HtmlEditorConfig::get('cms')->insertButtonsAfter('indent', ['|', 'undo', 'redo']);
+HtmlEditorConfig::get('cms')->setOption('content_css', 'cms/css/editor.css, mysite/css/editor.css?v=2');
