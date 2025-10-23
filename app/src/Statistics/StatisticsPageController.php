ed846110b83d16f44207c21ea43699bbf538c386
Bennet <zwibbltv@gmail.com>
2025-10-23 21:48:20 +0200

    [~] more statistics for registrations & feedback

--- app/src/Statistics/StatisticsPageController.php
+++ app/src/Statistics/StatisticsPageController.php
@@ -3,9 +3,13 @@
 namespace App\Statistics;
 
 use App\Events\EntryLog;
+use App\Events\Registration;
+use App\Feedback\FeedbackEntry;
 use PageController;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Model\List\GroupedList;
+use SilverStripe\Model\ArrayData;
+use SilverStripe\Model\List\ArrayList;
 use SilverStripe\Security\Security;
 
 /**
@@ -74,5 +78,108 @@ class StatisticsPageController extends PageController
     {
         $groupedList = GroupedList::create(EntryLog::get());
         return $groupedList;
+    }
+
+    
+
+    public function getOriginRegistrationThisYear(){
+        $currentYear = date('Y');
+        $startOfYear = date('Y-01-01 00:00:00', strtotime($currentYear.'-01-01'));
+        $endOfYear = date('Y-12-31 23:59:59', strtotime($currentYear.'-12-31'));
+        $registrations = Registration::get()->filter([
+            'Created:GreaterThanOrEqual' => $startOfYear,
+            'Created:LessThanOrEqual' => $endOfYear
+        ]);
+        $registrations = $registrations->sort(['ZIP' => 'ASC']);
+        $grouped = [];
+        foreach($registrations as $registration){
+            $plz = $registration->ZIP;
+            if(!isset($grouped[$plz])){
+                $grouped[$plz] = 0;
+            }
+            $grouped[$plz] += $registration->GroupSize;
+        }
+
+        arsort($grouped);
+        $result = [];
+        foreach ($grouped as $zip => $count) {
+            $result[] = ArrayData::create([
+                'ZIP' => $zip,
+                'Number' => $count
+            ]);
+        }
+        return ArrayList::create($result);
+    }
+
+    public function getOriginFeedbackThisYear(){
+        $currentYear = date('Y');
+        $startOfYear = date('Y-01-01 00:00:00', strtotime($currentYear.'-01-01'));
+        $endOfYear = date('Y-12-31 23:59:59', strtotime($currentYear.'-12-31'));
+        $feedbacks = FeedbackEntry::get()->filter([
+            'Created:GreaterThanOrEqual' => $startOfYear,
+            'Created:LessThanOrEqual' => $endOfYear
+        ]);
+        $feedbacks = $feedbacks->sort(['PLZ' => 'ASC']);
+        $grouped = [];
+        foreach($feedbacks as $feedback){
+            $plz = $feedback->PLZ;
+            if(!isset($grouped[$plz])){
+                $grouped[$plz] = 0;
+            }
+            $grouped[$plz] ++;
+        }
+
+        arsort($grouped);
+        $result = [];
+        foreach ($grouped as $zip => $count) {
+            $result[] = ArrayData::create([
+                'ZIP' => $zip,
+                'Number' => $count
+            ]);
+        }
+        return ArrayList::create($result);
+    }
+
+    public function getRatingPerDay() {
+        $currentYear = date('Y');
+        $startOfYear = date('Y-01-01 00:00:00', strtotime($currentYear . '-01-01'));
+        $endOfYear = date('Y-12-31 23:59:59', strtotime($currentYear . '-12-31'));
+        $feedbacks = FeedbackEntry::get()->filter([
+            'Created:GreaterThanOrEqual' => $startOfYear,
+            'Created:LessThanOrEqual' => $endOfYear
+        ]);
+
+        $days = [];
+        foreach ($feedbacks as $f) {
+            $d = $f->Day;
+            if (!isset($days[$d])) $days[$d] = ['stars' => 0, 'count' => 0];
+            $days[$d]['stars'] += $f->Stars;
+            $days[$d]['count']++;
+        }
+
+        krsort($days);
+
+        $result = [];
+        foreach ($days as $d => $v) {
+            $result[] = ArrayData::create([
+                'Day' => date('d.m.Y', strtotime($d)),
+                'AverageStars' => $v['count'] ? round($v['stars'] / $v['count'], 2) : 0,
+                'Count' => $v['count']
+            ]);
+        }
+        return ArrayList::create($result);
+    }
+
+    public function getFeedbackComments() {
+        $currentYear = date('Y');
+        $startOfYear = date('Y-01-01 00:00:00', strtotime($currentYear.'-01-01'));
+        $endOfYear = date('Y-12-31 23:59:59', strtotime($currentYear.'-12-31'));
+        $feedbacks = FeedbackEntry::get()->filter([
+            'Created:GreaterThanOrEqual' => $startOfYear,
+            'Created:LessThanOrEqual' => $endOfYear,
+            'Comment:Not' => ['', null]
+        ])->sort('Created', 'DESC');
+
+        return $feedbacks;
     }
 }
--- app/templates/App/Statistics/Layout/StatisticsPage.ss
+++ app/templates/App/Statistics/Layout/StatisticsPage.ss
@@ -10,6 +10,63 @@
         </div>
 
         <hr>
+        <h2>Registrierungen</h2>
+        <h3>Herkunft pro PLZ</h3>
+        <div class="statistics_row">
+            <% loop $OriginRegistrationThisYear %>
+                <div class="statistics_entry">
+                    <p class="entry_value">$ZIP</p>
+                    <p class="entry_title">$Number</p>
+                </div>
+            <% end_loop %>
+        </div>        
+
+        <h3>Registrierungen pro Tag</h3>
+        <div class="statistics_row" data-behaviour="stat_registrationsperday">
+            <div class="statistics_entry">
+                <p class="entry_value">???€</p>
+                <p class="entry_title">??.??.</p>
+            </div>
+        </div>
+
+        <div class="statistics_row">
+            <div class="statistics_entry">
+                <canvas id="dailyCharts"></canvas>
+            </div>
+        </div>
+        
+        <hr>
+        <h2>Feedback</h2>
+        <h3>Durchschnittliche Bewertung</h3>
+        <div class="statistics_row">
+            <% loop $RatingPerDay %>
+            <div class="statistics_entry">
+                <p class="entry_value">$AverageStars Sterne</p>
+                <p class="entry_title">$Day</p>
+            </div>
+            <% end_loop %>
+        </div>
+        
+        <h3>Herkunft pro PLZ</h3>
+        <div class="statistics_row">
+            <% loop $OriginFeedbackThisYear %>
+                <div class="statistics_entry">
+                    <p class="entry_value">$ZIP</p>
+                    <p class="entry_title">$Number</p>
+                </div>
+            <% end_loop %>
+        </div>
+
+        <h3>Kommentare</h3>
+        <div class="statistics_row">
+            <% loop $FeedbackComments %>
+                <div class="statistics_entry">
+                    <p class="entry_title">$Comment <br>$Day.Nice</p>
+                </div>
+            <% end_loop %>
+        </div>
+
+        <hr>
         <h2>Tägliche Statistiken</h2>
         <h3>Gäste pro Tag</h3>
         <div class="statistics_row" data-behaviour="stat_guestsperday">
@@ -32,20 +89,6 @@
             <div class="statistics_entry">
                 <p class="entry_value">???€</p>
                 <p class="entry_title">??.??.</p>
-            </div>
-        </div>
-
-        <h3>Registrierungen pro Tag</h3>
-        <div class="statistics_row" data-behaviour="stat_registrationsperday">
-            <div class="statistics_entry">
-                <p class="entry_value">???€</p>
-                <p class="entry_title">??.??.</p>
-            </div>
-        </div>
-
-        <div class="statistics_row">
-            <div class="statistics_entry">
-                <canvas id="dailyCharts"></canvas>
             </div>
         </div>
 
